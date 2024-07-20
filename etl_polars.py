bfe058fc1ac16345cea59ef89ff7d3d05c836c40
Pedro Carlos <pedrocarlos.assis@gmail.com>
2024-07-20 17:19:04 -0300

    Criação do arquivo etl_polars.py

--- etl_polars.py
+++ etl_polars.py
@@ -1,19 +1,25 @@
 import polars as pl
 
-# Created by Koen Vossen, 
-# Github: https://github.com/koenvo
-# Twitter/x Handle: https://twitter.com/mr_le_fox
-# https://x.com/mr_le_fox/status/1741893400947839362?s=20
 def create_polars_df():
     pl.Config.set_streaming_chunk_size(4000000)
+    
+    # Leia um pedaço do arquivo para verificar as colunas
+    preview_df = pl.read_csv("data/measurements.txt", separator=";", has_header=False)
+    print(preview_df.head())
+    
     return (
-        
-        pl.scan_csv("data/measurements.txt", separator=";", has_header=False, new_columns=["station", "measure"], schema={"station": pl.String, "measure": pl.Float64})
-        .group_by(by="station")
+        pl.scan_csv(
+            "data/measurements.txt",
+            separator=";",
+            has_header=False,
+            new_columns=["station", "measure"],
+            schema={"station": pl.Utf8, "measure": pl.Float64}
+        )
+        .group_by("station")
         .agg(
-            max = pl.col("measure").max(),
-            min = pl.col("measure").min(),
-            mean = pl.col("measure").mean()
+            max=pl.col("measure").max(),
+            min=pl.col("measure").min(),
+            mean=pl.col("measure").mean()
         )
         .sort("station")
         .collect(streaming=True)
